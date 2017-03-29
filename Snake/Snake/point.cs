b65fee5a9ca3252858434e837808714ac5dc57c4
Alex216 <projects216@gmail.com>
2017-03-29 14:10:12 +0300

    add Point

---
+++ Snake/Snake/point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()                       //так называемый процедурный метод
+                                                //эта ф-ия позволяет нам не копировать несколько раз нижеприведенный код, а выполняет его
+                                                //относительно количества вышенаписаных Draw
+        {
+            Console.SetCursorPosition(x, y);//указали координаты где выводить точку
+            Console.Write(sym);//выводим символ в координатах указаных сверху
+        }
+
+    }
+}
