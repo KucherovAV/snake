f5a3753c2c08ee220f411b8b1b7401f5776167b2
Alex216 <projects216@gmail.com>
2017-03-29 16:25:27 +0300

    Создаем список символов и выводим на экран

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '+');
-            p2.Draw();
+            Point p1 = new Point(4, 4, '*');
+            Point p2 = new Point(6, 6, '#');
+            Point p3 = new Point(8, 8, '$');
+            Point p4 = new Point(10, 10, '&');
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
 
             Console.ReadLine();
+
 
         }         
                      
