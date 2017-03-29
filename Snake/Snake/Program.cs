5924bc2ca15bfeee5ffc21eee55cb806f434eaf8
Alex216 <projects216@gmail.com>
2017-03-29 16:35:51 +0300

    Очень годная реализация списка

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(4, 4, '*');
-            Point p2 = new Point(6, 6, '#');
-            Point p3 = new Point(8, 8, '$');
-            Point p4 = new Point(10, 10, '&');
             List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            foreach (Point i in pList)
+
+            pList.Add(new Point(1, 3, '#'));
+            pList.Add(new Point(2, 3, '*'));
+            pList.Add(new Point(3, 3, '!'));
+            pList.Add(new Point(4, 3, '@'));
+
+            for (int i = 0; i < pList.Count; i++)
             {
-                i.Draw();
+                int z = i + 1;
+                Console.WriteLine("Точка " + z + " имеет координаты x:" + pList[i].x + " y:" + pList[i].y +
+                    " и символ \" " + pList[i].sym + "\""
+                    );
             }
+
 
             Console.ReadLine();
 
